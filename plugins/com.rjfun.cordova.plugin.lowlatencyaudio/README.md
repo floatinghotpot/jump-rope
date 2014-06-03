5488df8f146e3a30a3a09883db087ffc399af2f8
Liming Xie <liming.xie@gmail.com>
2014-06-03 21:19:35 +0800

    detect motion and count the number with voice

---
+++ plugins/com.rjfun.cordova.plugin.lowlatencyaudio/README.md
@@ -0,0 +1,24 @@
+cordova-plugin-lowlatencyaudio
+
+# LowLatencyAudio Plugin #
+---------------------------
+This is the LowLatencyAudio Cordova Plugin for Android and iOS.  
+
+The low latency audio plugin is designed to enable low latency and polyphonic audio from PhoneGap applications, using a very simple and basic API.
+
+It was developed by Andrew Trice - http://tricedesigns.com.
+
+You can read more about this plugin at:
+http://www.tricedesigns.com/2012/01/25/low-latency-polyphonic-audio-in-phonegap/
+
+Now it's ported to Cordova 3.0.0, and tested on Anroid and iOS.
+
+# How to use? #
+---------------------------
+To install this plugin, follow the [Command-line Interface Guide](http://cordova.apache.org/docs/en/edge/guide_cli_index.md.html#The%20Command-line%20Interface).
+
+It's recommended to use cordova command line tool: 
+
+cordova plugin add https://github.com/floatinghotpot/cordova-plugin-lowlatencyaudio.git
+
+Check the README.txt in sub folder for details.
---
+++ www/voice-count.js
@@ -0,0 +1,97 @@
+
+hotjs = hotjs || {};
+
+(function(){
+	
+//Method 1: get path using the last loaded script, 
+//remember, we must append script in resource preloading.
+function getCurrentScriptPath() {
+	var scripts = document.getElementsByTagName("script");
+	var n = scripts.length;
+	while( n > 0 ) {
+		n --;
+		var url = scripts[ n ].src;
+		if( url.indexOf('game.js') >= 0 ) return url;
+	}
+	return '';
+}
+
+//Method 2: get with error exception
+function getCurrentScriptPath2() {
+	var url = '';
+	try {
+		throw Error("get js path");
+	}catch(ex){
+		if(ex.fileName) { //Firefox
+			url = ex.fileName;
+		} else if(ex.sourceURL) { //Safari
+			url = ex.sourceURL;
+		} else if(ex.stack) { //Chrome or IE10+
+			url = (ex.stack.match(/at\s+(.*?):\d+:\d+/)||['',''])[1];
+		} else {
+			// no such info in ex, iOS 5
+		}
+	}
+	return url;
+}
+
+var __FILE__ = getCurrentScriptPath() || getCurrentScriptPath2();
+
+function _F(f) {
+	return hotjs.getAbsPath(f, __FILE__);
+}
+
+function _T(t) {
+	return hotjs.i18n.get(t);
+}
+
+var fx = {
+        '0':_F('audio/0.mp3'), 
+        '1':_F('audio/1.mp3'), 
+        '2':_F('audio/2.mp3'), 
+        '3':_F('audio/3.mp3'), 
+        '4':_F('audio/4.mp3'), 
+        '5':_F('audio/5.mp3'), 
+        '6':_F('audio/6.mp3'), 
+        '7':_F('audio/7.mp3'),
+        '8':_F('audio/8.mp3'),
+        '9':_F('audio/9.mp3'),
+        '10':_F('audio/10.mp3'),
+        '100':_F('audio/100.mp3'),
+        '1000':_F('audio/1000.mp3'),
+        '10000':_F('audio/10000.mp3')
+};
+
+var f = []; for ( var k in fx ) f.push( fx[k] );
+resources.preloadFX( f );	
+
+function voiceCount( num ) {
+	var numbers = [];
+	do {
+		numbers.push( num % 10 );
+		num = Math.floor( num / 10 );
+	} while (num > 0);
+
+    var i = 0;
+	while(numbers.length > 0) {
+        window.setTimeout(function( key ) {
+            resources.playAudio( fx[ key ], true );
+        }, i * 200, numbers.pop() + '' );
+        i ++;
+	}
+}
+
+function stopAllAudio() {
+	for ( var k in music ) {
+		resources.stopAudio( music[k] );
+	}
+	for ( var k in fx ) {
+		resources.stopAudio( fx[k] );
+	}
+}
+
+hotjs.voiceCount = voiceCount;
+hotjs.stopAllAudio = stopAllAudio;
+
+})();
+
